490131097d4fdfbea84ac02045cee7b9aa4acdf5
JaiRaga <raga.j.santhosh@gmail.com>
2022-03-30 14:40:47 +0530

    :zap: setup tailwindcss

---
+++ pages/home.js
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+
+function home() {
+	return (
+		<div>
+			<header>
+				<div className='relative w-36 h-10'>
+					<Image src='https://rb.gy/vtbzlp' layout='fill' objectFit='contain' />
+				</div>
+			</header>
+			<div className='flex items-center sm:divide-x divide-gray-300'>
+				<div>Pattu</div>
+				<div>Amma</div>
+			</div>
+		</div>
+	)
+}
+
+export default home
